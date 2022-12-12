d3d634cd1db623ca7a34f46655428001e9651453
Moshe Good <moshe@squareup.com>
2022-12-11 21:14:19 -0500

    2022 day 5

---
+++ 2022/day5/p2.pl
@@ -0,0 +1,36 @@
+use strict;
+use warnings;
+
+
+my @start = (
+	"",
+	"DBJV",
+	"PVBWRDF",
+	"RGFLDCWQ",
+	"WJPMLNDB",
+	"HNBPCSQ",
+	"RDBSNG",
+	"ZBPMQFSH",
+	"WLF",
+	"SVFMR"
+);
+
+foreach (@start) {
+    print "$_\n";
+}
+
+my $sum = 0;
+while (my $line = <>) {
+    chomp $line;
+    my ($cnt,$src,$dst) = $line =~ m/\d+/g;
+
+    print "$line\n";
+    $start[$dst] .= substr($start[$src],-$cnt);
+    substr($start[$src],-$cnt) = "";
+
+    print "NOW:\n";
+    foreach (@start) {
+        print "$_\n";
+    }
+}
+
